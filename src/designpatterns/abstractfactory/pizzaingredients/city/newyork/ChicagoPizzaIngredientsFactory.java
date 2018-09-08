7ba020d052c6925166826d14c5f16a47ab44315e
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-09-08 06:14:09 -0300

    defining factories of concrete products.

--- src/designpatterns/abstractfactory/pizzaingredients/city/newyork/ChicagoPizzaIngredientsFactory.java
+++ src/designpatterns/abstractfactory/pizzaingredients/city/newyork/ChicagoPizzaIngredientsFactory.java
@@ -1,5 +1,37 @@
 package designpatterns.abstractfactory.pizzaingredients.city.newyork;
 
-public class ChicagoPizzaIngredientsFactory {
+import designpatterns.abstractfactory.pizzaingredients.city.Cheese;
+import designpatterns.abstractfactory.pizzaingredients.city.Dough;
+import designpatterns.abstractfactory.pizzaingredients.city.Pepperon;
+import designpatterns.abstractfactory.pizzaingredients.city.PizzaIngredientsFactory;
+import designpatterns.abstractfactory.pizzaingredients.city.Sauce;
+import designpatterns.abstractfactory.pizzaingredients.city.Veggies;
+
+public class ChicagoPizzaIngredientsFactory implements PizzaIngredientsFactory {
+
+  @Override
+  public Cheese createCheese() {
+    return null;
+  }
+
+  @Override
+  public Dough createDough() {
+    return null;
+  }
+
+  @Override
+  public Pepperon createPepperon() {
+    return null;
+  }
+
+  @Override
+  public Sauce createSauce() {
+    return null;
+  }
+
+  @Override
+  public Veggies[] createVeggies() {
+    return null;
+  }
 
 }
