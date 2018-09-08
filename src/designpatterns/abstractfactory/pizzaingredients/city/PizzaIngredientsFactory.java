8c8655908087aeda638346f6b04508bb0b7ef897
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-09-08 06:05:48 -0300

    defining factory methods for the abstract factory.

--- src/designpatterns/abstractfactory/pizzaingredients/city/PizzaIngredientsFactory.java
+++ src/designpatterns/abstractfactory/pizzaingredients/city/PizzaIngredientsFactory.java
@@ -2,4 +2,14 @@ package designpatterns.abstractfactory.pizzaingredients.city;
 
 public interface PizzaIngredientsFactory {
 
+  Cheese createCheese();
+
+  Dough createDough();
+
+  Pepperon createPepperon();
+
+  Sauce createSauce();
+
+  Veggies[] createVeggies();
+
 }
