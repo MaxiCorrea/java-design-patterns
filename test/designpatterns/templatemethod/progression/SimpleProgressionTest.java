4e69c2fe036d7dce9dcc8dc819b9351938c57901
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-09-10 15:39:35 -0300

    Finish SimpleProgressionTest class

--- test/designpatterns/templatemethod/progression/SimpleProgressionTest.java
+++ test/designpatterns/templatemethod/progression/SimpleProgressionTest.java
@@ -6,8 +6,19 @@ import org.junit.Test;
 public class SimpleProgressionTest {
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void testProgressionFromZeroToTen() {
+    SimpleProgression progression = new SimpleProgression();
+    assertEquals(0 , progression.nextValue());
+    assertEquals(1 , progression.nextValue());
+    assertEquals(2 , progression.nextValue());
+    assertEquals(3 , progression.nextValue());
+    assertEquals(4 , progression.nextValue());
+    assertEquals(5 , progression.nextValue());
+    assertEquals(6 , progression.nextValue());
+    assertEquals(7 , progression.nextValue());
+    assertEquals(8 , progression.nextValue());
+    assertEquals(9 , progression.nextValue());
+    assertEquals(10 , progression.nextValue());
   }
 
 }
