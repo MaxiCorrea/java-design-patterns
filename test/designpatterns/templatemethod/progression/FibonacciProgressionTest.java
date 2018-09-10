03d4c1e9b86e1b3b8d762ce4883dda911e9ffcda
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-09-10 16:02:17 -0300

    Finish FibonacciProgressionTest class

--- test/designpatterns/templatemethod/progression/FibonacciProgressionTest.java
+++ test/designpatterns/templatemethod/progression/FibonacciProgressionTest.java
@@ -6,8 +6,16 @@ import org.junit.Test;
 public class FibonacciProgressionTest {
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void testFibonacciProgression() {
+    FibonacciProgression progression = new FibonacciProgression();
+    assertEquals(0 , progression.nextValue());
+    assertEquals(1 , progression.nextValue());
+    assertEquals(1 , progression.nextValue());
+    assertEquals(2 , progression.nextValue());
+    assertEquals(3 , progression.nextValue());
+    assertEquals(5 , progression.nextValue());
+    assertEquals(8 , progression.nextValue());
+    assertEquals(13 , progression.nextValue());    
   }
 
 }
