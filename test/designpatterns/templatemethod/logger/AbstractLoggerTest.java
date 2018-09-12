960bcf3102941cdd1b5af976877e7ec34016d8ca
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-09-12 17:51:34 -0300

    defining unit test with the SELFT-SHUNT pattern.

--- test/designpatterns/templatemethod/logger/AbstractLoggerTest.java
+++ test/designpatterns/templatemethod/logger/AbstractLoggerTest.java
@@ -1,13 +1,50 @@
 package designpatterns.templatemethod.logger;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
-public class AbstractLoggerTest {
+public class AbstractLoggerTest extends AbstractLogger {
 
+  private boolean openDataStoreOperationCalled;
+  private boolean logMessageCalled;
+  private boolean closeDataStoreOperationCalled;
+  
+  public AbstractLoggerTest() {
+    super(false);
+  }
+
+  @Before
+  public void setup() {
+    openDataStoreOperationCalled = false;
+    logMessageCalled = false;
+    closeDataStoreOperationCalled = false;
+  }
+  
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void shouldCallTheTemplateMethods() {
+    assertFalse(openDataStoreOperationCalled);
+    assertFalse(logMessageCalled);
+    assertFalse(closeDataStoreOperationCalled);
+    log("");
+    assertTrue(openDataStoreOperationCalled);
+    assertTrue(logMessageCalled);
+    assertTrue(closeDataStoreOperationCalled);    
+  }
+
+  @Override
+  void openDataStoreOperation() {
+   openDataStoreOperationCalled = true; 
+  }
+
+  @Override
+  void logMessage(String messageToLog) {
+   logMessageCalled = true;
+  }
+
+  @Override
+  void closeDataStoreOperation() {
+    closeDataStoreOperationCalled = true;
   }
 
 }
