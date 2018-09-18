9c18eee29920ba6c85cd45b9de6b49d6668ec4bc
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-09-18 05:04:57 -0300

    Refactor : rename method

--- test/designpatterns/prototype/ConcretePrototype2Test.java
+++ test/designpatterns/prototype/ConcretePrototype2Test.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class ConcretePrototype2Test {
 
   @Test
-  public void testCopyOFSelf() {
+  public void testCopyOfSelf() {
     ConcretePrototype2 prototype = new ConcretePrototype2("A");
     ConcretePrototype2 copy = prototype.copyOfSelf();
     assertEquals(copy.getStr() , prototype.getStr());
