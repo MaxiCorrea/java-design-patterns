fc299145bacd00b380704882f6e9726f36a78f62
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-09-21 02:28:08 -0300

    creating the first unit tests.

---
+++ test/designpatterns/adapter/dialler/ButtonTest.java
@@ -0,0 +1,13 @@
+package designpatterns.adapter.dialler;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ButtonTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
