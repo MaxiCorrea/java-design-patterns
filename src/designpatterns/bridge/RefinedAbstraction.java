be30247cf7438bae6bcb673bb87916d38daa3727
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-09-22 02:46:14 -0300

    Defining subclass constructor

--- src/designpatterns/bridge/RefinedAbstraction.java
+++ src/designpatterns/bridge/RefinedAbstraction.java
@@ -2,6 +2,10 @@ package designpatterns.bridge;
 
 public class RefinedAbstraction extends Abstraction {
 
+  public RefinedAbstraction(Implementor implementor) {
+    super(implementor);
+  }
+
   @Override
   public void operation() {
     
