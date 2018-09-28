8f14a3b8f6b3d0c4ef39af1fff914299d72953fa
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-09-28 19:01:45 -0300

    Definition of test classes for the decorator pattern.

---
+++ test/designpatterns/decorator/ComponentTest.java
@@ -0,0 +1,13 @@
+package designpatterns.decorator;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ComponentTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
