2a2f6824b9246f81c7a6f29815a0981ac52cc4aa
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-09-28 20:43:42 -0300

    Refactor : Rename class

--- src/designpatterns/decorator/starbuzz/Espresso.java
+++ src/designpatterns/decorator/starbuzz/Espresso.java
@@ -1,8 +1,8 @@
 package designpatterns.decorator.starbuzz;
 
-public class Expresso extends Beverage {
+public class Espresso extends Beverage {
 
-  public Expresso() {
+  public Espresso() {
     super("Espresso");
   }
   
