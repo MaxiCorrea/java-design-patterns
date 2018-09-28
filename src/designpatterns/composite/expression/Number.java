62b806dff9f524bf688f9dc4bc5d781096700b9e
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-09-28 10:29:09 -0300

    Simple Format

--- src/designpatterns/composite/expression/Number.java
+++ src/designpatterns/composite/expression/Number.java
@@ -5,19 +5,19 @@ import static java.lang.String.valueOf;
 public class Number extends Expression {
 
   private int value;
-  
-  public Number( int value) {
+
+  public Number(int value) {
     this.value = value;
   }
-  
+
   @Override
   public int operate() {
     return value;
   }
-  
+
   @Override
   public String toString() {
     return valueOf(value);
   }
-  
+
 }
