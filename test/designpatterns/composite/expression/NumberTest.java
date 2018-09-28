ad84bfcfc19530ea7753bbc58bef88757458f202
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-09-28 10:28:30 -0300

    Testing the toString method

--- test/designpatterns/composite/expression/NumberTest.java
+++ test/designpatterns/composite/expression/NumberTest.java
@@ -6,9 +6,15 @@ import org.junit.Test;
 public class NumberTest {
 
   @Test
-  public void test() {
+  public void testOperation() {
     Number n = new Number(5);
-    assertEquals(5 , n.operate());
+    assertEquals(5, n.operate());
+  }
+
+  @Test
+  public void testToString() {
+    Number n = new Number(5);
+    assertEquals("5", n.toString());
   }
 
 }
