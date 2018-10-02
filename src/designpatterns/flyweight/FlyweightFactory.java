ac8fa56ecf635f52a12374f7ec35b55b217228be
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-10-02 15:29:50 -0300

    Definition of FlyweightFactory class

---
+++ src/designpatterns/flyweight/FlyweightFactory.java
@@ -0,0 +1,29 @@
+package designpatterns.flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlyweightFactory {
+
+  private final Map<String, Flyweight> pool;
+
+  public FlyweightFactory() {
+    pool = new HashMap<>();
+  }
+
+  public Flyweight getFlyweight(String key) {
+    Flyweight flyweight = pool.get(key);
+    if (flyweight == null) {
+      if ("ConcreteFlyweight".equals(key)) {
+        flyweight = new ConcreteFlyweight();
+      } else if (key.equals("UnsharedConcreteFlyweiht")) {
+        flyweight = new UnsharedConcreteFlyweight();
+      } else {
+        throw new IllegalArgumentException("Invalid key : " + key);
+      }
+      pool.put(key, flyweight);
+    }
+    return flyweight;
+  }
+
+}
