f5b3200332c247a7c27f563e5fe57ca3687f6b96
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-10-04 17:12:17 -0300

    Implementation of the TreeType class.

--- src/designpatterns/flyweight/forest/TreeType.java
+++ src/designpatterns/flyweight/forest/TreeType.java
@@ -1,12 +1,31 @@
 package designpatterns.flyweight.forest;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class TreeType {
 
+  private final String name;
+  private final Color color;
+  
+  public TreeType(String name , Color color) {
+    this.name = name;
+    this.color = color;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public Color getColor() {
+    return color;
+  }
+  
   public void draw(Graphics graphics, int x, int y) {
-    // TODO Auto-generated method stub
-    
+    graphics.setColor(Color.BLACK);
+    graphics.fillRect(x - 1, y, 3, 5);
+    graphics.setColor(getColor());
+    graphics.fillOval(x - 5, y - 10, 10, 10);
   }
 
 }
