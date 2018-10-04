0b1b3584878f7a758c18dd2d8e14444c5c079205
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-10-04 17:08:39 -0300

    Implementation of the Tree class.

--- src/designpatterns/flyweight/forest/TreeType.java
+++ src/designpatterns/flyweight/forest/TreeType.java
@@ -1,5 +1,12 @@
 package designpatterns.flyweight.forest;
 
+import java.awt.Graphics;
+
 public class TreeType {
+
+  public void draw(Graphics graphics, int x, int y) {
+    // TODO Auto-generated method stub
+    
+  }
 
 }
