23ca2529140ba8dd52865c8b58cc2acc2b39d6ef
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-10-04 17:15:43 -0300

    Implementation of the TreeFactory class.

--- src/designpatterns/flyweight/forest/TreeFactory.java
+++ src/designpatterns/flyweight/forest/TreeFactory.java
@@ -1,5 +1,24 @@
 package designpatterns.flyweight.forest;
 
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TreeFactory {
+
+  private Map<String, TreeType> pool;
+
+  public TreeFactory() {
+    pool = new HashMap<>();
+  }
+
+  public TreeType getTreeType(String name , Color color) {
+    TreeType treeType = pool.get(name);
+    if(treeType == null) {
+      treeType = new TreeType(name, color);
+      pool.put(name , treeType);
+    }
+    return treeType;
+  }
 
 }
